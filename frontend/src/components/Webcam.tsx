03d7941444fdf0613d34e2928278b2677fa814fe
Prasanna <iprasannamb@gmail.com>
2026-01-30 09:04:45 +0530

    Update switch icon
    Signed-off-by: Prasanna <iprasannamb@gmail.com>

--- frontend/src/components/Webcam.tsx
+++ frontend/src/components/Webcam.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from "react";
-import { CameraIcon, XMarkIcon, ArrowPathIcon, CheckIcon, ArrowPathRoundedSquareIcon } from "@heroicons/react/24/outline";
+import { CameraIcon, XMarkIcon, ArrowPathIcon, CheckIcon } from "@heroicons/react/24/outline";
 import toast from "react-hot-toast";
 
 interface WebcamProps {
@@ -286,7 +286,20 @@ const Webcam = ({ onCapture, onClose }: WebcamProps) => {
           className="absolute top-4 left-4 p-2 bg-white rounded-full text-black shadow-lg hover:bg-gray-100 transition-colors z-10"
           title="Switch Camera"
         >
-          <ArrowPathRoundedSquareIcon className="h-6 w-6" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="h-6 w-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+            />
+          </svg>
         </button>
       )}
 
